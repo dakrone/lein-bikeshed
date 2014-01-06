6e76e79b5fb46038eebe6b8c6d03b55e3aed8c33
Lee Hinman <lee@writequit.org>
2014-01-05 20:53:06 -0700

    Update .gitignore

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-bikeshed "0.1.4-SNAPSHOT"
-  :description "A Leiningen plugin designed to tell you your code is bad, and that you should feel bad"
+  :description (str "A Leiningen plugin designed to tell you your code is bad, "
+                    "and that you should feel bad")
   :url "https://github.com/dakrone/lein-bikeshed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
