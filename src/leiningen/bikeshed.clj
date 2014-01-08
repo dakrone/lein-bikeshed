0788a91c581eb6a30c7f620e7b585b13a35a1b94
Lee Hinman <lee@writequit.org>
2014-01-08 08:19:14 -0700

    require c.t.cli in lein init block and use fully-qualified

--- src/leiningen/bikeshed.clj
+++ src/leiningen/bikeshed.clj
@@ -1,5 +1,4 @@
 (ns leiningen.bikeshed
-  (:use [clojure.tools.cli :only (cli)])
   (:require [leiningen.core.eval :as lein]))
 
 (defn bikeshed [project & args]
@@ -8,13 +7,14 @@
    (-> project
        (update-in [:dependencies] conj ['lein-bikeshed "0.1.5-SNAPSHOT"]))
    `(let [[opts# args# banner#]
-          (cli '~args
-               ["-H" "--help-me" "Show help"
-                :flag true :default false]
-               ["-v" "--verbose" "Display missing doc strings"
-                :flag true :default false]
-               ["-m" "--max-line-length" "Max line length"
-                :default nil])]
+          (clojure.tools.cli/cli
+           '~args
+           ["-H" "--help-me" "Show help"
+            :flag true :default false]
+           ["-v" "--verbose" "Display missing doc strings"
+            :flag true :default false]
+           ["-m" "--max-line-length" "Max line length"
+            :default nil])]
       '~project
       (when (:help-me opts#)
         (println banner#)
@@ -24,4 +24,6 @@
                       :verbose (:verbose opts#)})
         (System/exit -1)
         (System/exit 0)))
-   '(require 'bikeshed.core)))
+   '(do
+      (require 'bikeshed.core)
+      (require 'clojure.tools.cli))))
