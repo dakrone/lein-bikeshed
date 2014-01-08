348159d09f5872b742d4d839665d4e8e6fc5585c
Lee Hinman <lee@writequit.org>
2014-01-08 09:27:09 -0700

    Release 0.1.6 ಠ_ಠ

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ should feel bad.
 Add to your ~/.lein/profiles.clj:
 
 ```clojure
-{:user {:plugins [[lein-bikeshed "0.1.5"]]}}
+{:user {:plugins [[lein-bikeshed "0.1.6"]]}}
 ```
 
 Just run `lein bikeshed` on your project:
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bikeshed "0.1.6-SNAPSHOT"
+(defproject lein-bikeshed "0.1.6"
   :description (str "A Leiningen plugin designed to tell you your code is bad, "
                     "and that you should feel bad")
   :url "https://github.com/dakrone/lein-bikeshed"
