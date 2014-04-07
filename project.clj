fbb176a249344c32ffc5c5522e3ddd7abd29b7a9
Lee Hinman <lee@writequit.org>
2014-04-06 22:35:42 -0600

    Release 0.1.7

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-bikeshed "0.1.7-SNAPSHOT"
+(defproject lein-bikeshed "0.1.7"
   :description (str "A Leiningen plugin designed to tell you your code is bad, "
                     "and that you should feel bad")
   :url "https://github.com/dakrone/lein-bikeshed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in :leiningen
-  :dependencies [[org.clojure/tools.cli       "0.2.4"]
-                 [org.clojure/tools.namespace "0.2.3"]])
+  :dependencies [[org.clojure/tools.cli       "0.3.1"]
+                 [org.clojure/tools.namespace "0.2.4"]])
