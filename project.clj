fee7fe476a37435efb811362589273d2b11edf81
Lee Hinman <lee@writequit.org>
2016-11-17 15:58:19 -0700

    Bump to 0.4.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bikeshed "0.4.1"
+(defproject lein-bikeshed "0.4.2-SNAPSHOT"
   :description (str "A Leiningen plugin designed to tell you your code is bad, "
                     "and that you should feel bad")
   :url "https://github.com/dakrone/lein-bikeshed"
