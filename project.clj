bb94a654b13440413a8d003af2ddca30ec27ab0b
Immo Heikkinen <immo.heikkinen@zalando.fi>
2018-01-15 12:45:57 +0200

    Add org.clojure/tools.cli dependency back
    This was accidentally removed in #31.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in :leiningen
-  :dependencies [[org.clojure/tools.namespace "0.2.6"]])
+  :dependencies [[org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/tools.namespace "0.2.6"]])
