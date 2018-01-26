627d355d84700b2920ea2722ce191140e3f53d65
Lee Hinman <lee@writequit.org>
2018-01-26 13:51:59 -0700

    Add a test in the test dir that shouldn't trigger the with-redefs check

--- test/bikeshed/core_test.clj
+++ test/bikeshed/core_test.clj
@@ -6,6 +6,9 @@
   (testing "FIXME, I fail, and I have trailing whitespace!"   
     (is (= 0 1))))
 
+(deftest another-test
+  (with-redefs [get get]
+    (is (= 2 2))))
 
 (def this-thing-is-over-eighty-characters-long "yep, it certainly is over eighty characters long")
 
